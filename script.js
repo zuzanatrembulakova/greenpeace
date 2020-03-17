a355c593035dfea9b0dba6d7e5fa4d5f91837984
zuzanatrembulakova <zuzanatrembulakova@users.noreply.github.com>
2020-03-17 15:23:59 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//set up burger menu handler
+const navbar = document.getElementById("navbar");
+if (document.querySelector(".burger_menu") != null) {
+    const bmenu = document.querySelector(".burger_menu");
+    bmenu.addEventListener("click", function () {
+        if (navbar.style.display === "block") {
+            navbar.style.display = "none";
+        } else {
+            navbar.style.display = "block";
+        }
+    })
+} else {
+    navbar.style.display = "block";
+};
